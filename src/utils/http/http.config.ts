2e436c19b97aa1c1cd49aa73e20e61118e447ca6
AhYaaaaas <xuanyige87@gmail.com>
2022-10-22 23:36:56 +0800

    try

--- src/utils/http/http.config.ts
+++ src/utils/http/http.config.ts
@@ -1,7 +1,7 @@
 /*
  * @Date: 2022-10-21 19:40:04
- * @LastEditors: xuanyi_ge xuanyige87@gmail.com
- * @LastEditTime: 2022-10-22 22:25:17
+ * @LastEditors: AhYaaaaas xuanyige87@gmail.com
+ * @LastEditTime: 2022-10-22 23:36:45
  * @FilePath: \NodeReactProject-FE\src\utils\http\http.config.ts
  */
 import {CreateAxiosDefaults} from "axios"
