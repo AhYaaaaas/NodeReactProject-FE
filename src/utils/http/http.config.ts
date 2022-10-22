08e669845b623f49811052fa2044bed8e1571a45
xuanyi_ge <xuanyige87@gmail.com>
2022-10-22 23:34:29 +0800

    try

--- src/utils/http/http.config.ts
+++ src/utils/http/http.config.ts
@@ -1,12 +1,12 @@
 /*
  * @Date: 2022-10-21 19:40:04
  * @LastEditors: xuanyi_ge xuanyige87@gmail.com
- * @LastEditTime: 2022-10-22 12:26:51
+ * @LastEditTime: 2022-10-22 22:25:17
  * @FilePath: \NodeReactProject-FE\src\utils\http\http.config.ts
  */
 import {CreateAxiosDefaults} from "axios"
 export const httpConfig: CreateAxiosDefaults<any> = {
-  baseURL: "http://127.0.0.1:8080",
+  baseURL: "http://localhost:5000",
   timeout: 5000,
   headers: {
   }
